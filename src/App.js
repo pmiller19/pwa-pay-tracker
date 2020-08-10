eec401ad89bbee7cfde326e958e69b4258bf7d9c
Parker Miller <pmiller19@huskers.unl.edu>
2020-08-09 22:34:24 -0500

    testing if auto updates to the app work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { fetchWeather } from './api/fetchWeather';
+// import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 const App = () => {
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
+    // const [query, setQuery] = useState('');
+    // const [weather, setWeather] = useState({});
 
 
-    const search = async (e) => {
-        if (e.key === 'Enter') {
-            const data = await fetchWeather(query)
+    // const search = async (e) => {
+    //     if (e.key === 'Enter') {
+    //         const data = await fetchWeather(query)
 
-            setWeather(data);
-            setQuery('');
-        }
-    }
+    //         setWeather(data);
+    //         setQuery('');
+    //     }
+    // }
     return (
         <div className="main-container">
             {/* <input type="text" className="search" placeHolder="Search..." value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
